bf1d1b44af0f3503f3d4f4dd1f809f71ff37b431
uturkarslan <ugurturkarslan@gmail.com>
2023-01-22 19:42:29 +0300

    Implement fetchDomainIds tests

--- pkg/users/users_test.go
+++ pkg/users/users_test.go
@@ -3,13 +3,22 @@ package users
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
 	"testing"
 
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/utr1903/newrelic-tracker-user/pkg/graphql/user"
+)
+
+const (
+	dom1 = "dom1"
+	dom2 = "dom2"
+
+	dom1user1 = "dom1user1"
+	dom1user2 = "dom1user2"
+	dom2user1 = "dom2user1"
+	dom2user2 = "dom2user2"
 )
 
 type loggerMock struct {
@@ -34,11 +43,11 @@ func (l *loggerMock) Flush() error {
 	return nil
 }
 
-type graphqlClientMock struct {
+type graphqlClientMockDomains struct {
 	failRequest bool
 }
 
-func (c *graphqlClientMock) Execute(
+func (c *graphqlClientMockDomains) Execute(
 	qv any,
 	result any,
 ) error {
@@ -46,13 +55,73 @@ func (c *graphqlClientMock) Execute(
 		return errors.New("error")
 	}
 
-	authDomains := getAuthDomains(qv)
+	qvParsed := parseQueryVariablesDomains(qv)
+
+	var authDomainsResponse user.AuthenticationDomains
+	if qvParsed.Cursor == "null" {
+		nextCursor := "notnull"
+		authDomainsResponse = user.AuthenticationDomains{
+			NextCursor: &nextCursor,
+			AuthenticationDomains: []user.AuthenticationDomain{
+				{
+					Id: dom1,
+				},
+			},
+		}
+	} else {
+		authDomainsResponse = user.AuthenticationDomains{
+			NextCursor: nil,
+			AuthenticationDomains: []user.AuthenticationDomain{
+				{
+					Id: dom2,
+				},
+			},
+		}
+	}
+
 	res := user.GraphQlUserResponse{
 		Data: user.Data{
 			Actor: user.Actor{
 				Organization: user.Organization{
 					UserManagement: user.UserManagement{
-						AuthenticationDomains: *authDomains,
+						AuthenticationDomains: authDomainsResponse,
+					},
+				},
+			},
+		},
+	}
+
+	bytes, err := json.Marshal(res)
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(bytes, result)
+	if err != nil {
+		panic(err)
+	}
+
+	return nil
+}
+
+type graphqlClientMockUsers struct {
+	failRequest bool
+}
+
+func (c *graphqlClientMockUsers) Execute(
+	qv any,
+	result any,
+) error {
+	if c.failRequest {
+		return errors.New("error")
+	}
+
+	res := user.GraphQlUserResponse{
+		Data: user.Data{
+			Actor: user.Actor{
+				Organization: user.Organization{
+					UserManagement: user.UserManagement{
+						AuthenticationDomains: user.AuthenticationDomains{},
 					},
 				},
 			},
@@ -93,112 +162,119 @@ func (mf *metricForwarderMock) Run() error {
 	return nil
 }
 
-func Test_FetchingFails(t *testing.T) {
+func Test_FetchingDomainsFails(t *testing.T) {
 	logger := newLoggerMock()
-	gqlc := &graphqlClientMock{
+	gqlcDomains := &graphqlClientMockDomains{
+		failRequest: true,
+	}
+	gqlcUsers := &graphqlClientMockUsers{
 		failRequest: true,
 	}
 	mf := &metricForwarderMock{
 		returnError: true,
 	}
 
-	uas := &Users{
+	us := &Users{
 		OrganizationId:  "organizationId",
 		Logger:          logger,
-		Gqlc:            gqlc,
+		GqlcDomains:     gqlcDomains,
+		GqlcUsers:       gqlcUsers,
 		MetricForwarder: mf,
 	}
 
-	err := uas.Run()
+	err := us.Run()
 
 	assert.Nil(t, err)
 }
 
-func Test_FetchingSucceeds(t *testing.T) {
+func Test_FetchingDomainsSucceeds(t *testing.T) {
 	logger := newLoggerMock()
-	gqlc := &graphqlClientMock{
+	gqlcDomains := &graphqlClientMockDomains{
 		failRequest: false,
+	}
+	gqlcUsers := &graphqlClientMockUsers{
+		failRequest: true,
 	}
 	mf := &metricForwarderMock{
 		returnError: true,
 	}
 
-	users := &Users{
+	us := &Users{
 		OrganizationId:  "organizationId",
 		Logger:          logger,
-		Gqlc:            gqlc,
+		GqlcDomains:     gqlcDomains,
+		GqlcUsers:       gqlcUsers,
 		MetricForwarder: mf,
 	}
 
-	_, err := users.fetchUsers()
-	if err != nil {
-		panic(err)
-	}
+	_, err := us.fetchDomainIds()
 
-	// authDomainUsers, err := users.fetchUsers()
-
-	// for _, authDomainUser := range authDomainUsers {
-	// 	fmt.Println(authDomainUser)
-	// }
-	// assert.Nil(t, err)
-	// assert.NotNil(t, authDomainUsers)
-
-	// for i, appName := range appNames {
-	// 	assert.Equal(t, apps[i], appName)
-	// }
+	assert.Nil(t, err)
 }
 
-// func Test_FlushingFails(t *testing.T) {
-// 	logger := newLoggerMock()
-// 	gqlc := &graphqlClientMock{
-// 		failRequest: false,
-// 	}
-// 	mf := &metricForwarderMock{
-// 		returnError: true,
-// 	}
+func createAuthDomainUsersMock() map[string](map[string]user.User) {
 
-// 	uas := &Users{
-// 		OrganizationId:  "organizationId",
-// 		Logger:          logger,
-// 		Gqlc:            gqlc,
-// 		MetricForwarder: mf,
-// 	}
+	return map[string](map[string]user.User){
+		dom1: map[string]user.User{
+			dom1user1: user.User{
+				Id:   dom1user1,
+				Name: dom1user1,
+				UserType: user.UserType{
+					Id: "1",
+				},
+				Email:                  dom1user1,
+				EmailVerificationState: "Verified",
+				LastActive:             "2022-10-11T10:10:05Z",
+				TimeZone:               "Etc/UTC",
+			},
+			dom1user2: user.User{
+				Id:   dom1user2,
+				Name: dom1user2,
+				UserType: user.UserType{
+					Id: "0",
+				},
+				Email:                  dom1user2,
+				EmailVerificationState: "Verified",
+				LastActive:             "2022-10-11T10:10:05Z",
+				TimeZone:               "Etc/UTC",
+			},
+		},
+		dom2: map[string]user.User{
+			dom2user1: user.User{
+				Id:   dom2user1,
+				Name: dom2user1,
+				UserType: user.UserType{
+					Id: "0",
+				},
+				Email:                  dom2user1,
+				EmailVerificationState: "Verified",
+				LastActive:             "2022-10-11T10:10:05Z",
+				TimeZone:               "Etc/UTC",
+			},
+			dom2user2: user.User{
+				Id:   dom2user2,
+				Name: dom2user2,
+				UserType: user.UserType{
+					Id: "0",
+				},
+				Email:                  dom2user2,
+				EmailVerificationState: "Verified",
+				LastActive:             "2022-10-11T10:10:05Z",
+				TimeZone:               "Etc/UTC",
+			},
+		},
+	}
+}
 
-// 	err := uas.Run()
-
-// 	assert.Nil(t, err)
-// }
-
-// func Test_FlushingSucceeds(t *testing.T) {
-// 	logger := newLoggerMock()
-// 	gqlc := &graphqlClientMock{
-// 		failRequest: false,
-// 	}
-// 	mf := &metricForwarderMock{
-// 		returnError: false,
-// 	}
-
-// 	uas := &Users{
-// 		OrganizationId:  "organizationId",
-// 		Logger:          logger,
-// 		Gqlc:            gqlc,
-// 		MetricForwarder: mf,
-// 	}
-
-// 	err := uas.Run()
-
-// 	assert.Nil(t, err)
-// }
-
-func parseQueryVariables(
+func parseQueryVariablesDomains(
 	qv any,
-) *queryVariables {
+) *queryVariablesDomains {
 	bytes, err := json.Marshal(qv)
 	if err != nil {
 		panic(err)
 	}
 
-	qvParsed := &queryVariables{}
+	qvParsed := &queryVariablesDomains{}
 	err = json.Unmarshal(bytes, qvParsed)
 	if err != nil {
 		panic(err)
@@ -206,167 +282,18 @@ func parseQueryVariables(
 	return qvParsed
 }
 
-func getAuthDomains(
+func parseQueryVariablesUsers(
 	qv any,
-) *user.AuthenticationDomains {
-
-	qvParsed := parseQueryVariables(qv)
-
-	// fmt.Println("DOMAIN: " + qvParsed.CursorDomain + " || USER: " + qvParsed.CursorDomain)
-
-	// First call
-	if qvParsed.CursorDomain == "null" && qvParsed.CursorUser == "null" {
-		fmt.Println("--- 1 ---")
-		nextCursorUser := "notnull"
-		return &user.AuthenticationDomains{
-			NextCursor: nil,
-			AuthenticationDomains: []user.AuthenticationDomain{
-				getAuthDomain1(&nextCursorUser),
-			},
-		}
-	}
-	// Second call
-	if qvParsed.CursorDomain == "null" && qvParsed.CursorUser == "notnull" {
-		fmt.Println("--- 2 ---")
-		nextCursorDomain := "notnull"
-		return &user.AuthenticationDomains{
-			NextCursor: &nextCursorDomain,
-			AuthenticationDomains: []user.AuthenticationDomain{
-				getAuthDomain1(nil),
-			},
-		}
-	}
-	// Third call
-	if qvParsed.CursorDomain == "notnull" && qvParsed.CursorUser == "null" {
-		fmt.Println("--- 3 ---")
-		nextCursorUser := "notnull"
-		return &user.AuthenticationDomains{
-			NextCursor: nil,
-			AuthenticationDomains: []user.AuthenticationDomain{
-				getAuthDomain2(&nextCursorUser),
-			},
-		}
-	}
-	// Fourth call
-	if qvParsed.CursorDomain == "null" && qvParsed.CursorUser == "nill" {
-		fmt.Println("--- 4 ---")
-		return &user.AuthenticationDomains{
-			NextCursor: nil,
-			AuthenticationDomains: []user.AuthenticationDomain{
-				getAuthDomain2(nil),
-			},
-		}
-	}
-	fmt.Println("--- NONE ---")
-	return nil
-}
-
-func getAuthDomain1(
-	nextCursorUser *string,
-) user.AuthenticationDomain {
-	dom := "authDomain1"
-
-	users := []user.User{}
-	if nextCursorUser != nil {
-		users = append(users, getAuthDomain1User1(dom))
-	} else {
-		users = append(users, getAuthDomain1User2(dom))
+) *queryVariablesUsers {
+	bytes, err := json.Marshal(qv)
+	if err != nil {
+		panic(err)
 	}
 
-	authDomain1Users := user.Users{
-		NextCursor: nextCursorUser,
-		Users:      users,
+	qvParsed := &queryVariablesUsers{}
+	err = json.Unmarshal(bytes, qvParsed)
+	if err != nil {
+		panic(err)
 	}
-	return user.AuthenticationDomain{
-		Id:    dom,
-		Name:  dom,
-		Users: authDomain1Users,
-	}
-}
-
-func getAuthDomain2(
-	nextCursorUser *string,
-) user.AuthenticationDomain {
-	dom := "authDomain2"
-
-	users := []user.User{}
-	if nextCursorUser != nil {
-		users = append(users, getAuthDomain2User1(dom))
-	} else {
-		users = append(users, getAuthDomain2User2(dom))
-	}
-
-	authDomain2Users := user.Users{
-		NextCursor: nextCursorUser,
-		Users:      users,
-	}
-	return user.AuthenticationDomain{
-		Id:    dom,
-		Name:  dom,
-		Users: authDomain2Users,
-	}
-}
-
-func getAuthDomain1User1(
-	authDomain1Id string,
-) user.User {
-	return user.User{
-		Id:   authDomain1Id + "user1",
-		Name: authDomain1Id + "user1",
-		UserType: user.UserType{
-			Id: "0",
-		},
-		Email:                  authDomain1Id + "email1",
-		EmailVerificationState: "Verified",
-		LastActive:             "2022-10-11T10:10:05Z",
-		TimeZone:               "Etc/UTC",
-	}
-}
-
-func getAuthDomain1User2(
-	authDomain1Id string,
-) user.User {
-	return user.User{
-		Id:   authDomain1Id + "user2",
-		Name: authDomain1Id + "user2",
-		UserType: user.UserType{
-			Id: "1",
-		},
-		Email:                  authDomain1Id + "email2",
-		EmailVerificationState: "Verified",
-		LastActive:             "2022-10-11T10:10:05Z",
-		TimeZone:               "Etc/UTC",
-	}
-}
-
-func getAuthDomain2User1(
-	authDomain2Id string,
-) user.User {
-	return user.User{
-		Id:   authDomain2Id + "user1",
-		Name: authDomain2Id + "user1",
-		UserType: user.UserType{
-			Id: "0",
-		},
-		Email:                  authDomain2Id + "email1",
-		EmailVerificationState: "Verified",
-		LastActive:             "2022-10-11T10:10:05Z",
-		TimeZone:               "Etc/UTC",
-	}
-}
-
-func getAuthDomain2User2(
-	authDomain2Id string,
-) user.User {
-	return user.User{
-		Id:   authDomain2Id + "user2",
-		Name: authDomain2Id + "user2",
-		UserType: user.UserType{
-			Id: "0",
-		},
-		Email:                  authDomain2Id + "email2",
-		EmailVerificationState: "Verified",
-		LastActive:             "2022-10-11T10:10:05Z",
-		TimeZone:               "Etc/UTC",
-	}
+	return qvParsed
 }
